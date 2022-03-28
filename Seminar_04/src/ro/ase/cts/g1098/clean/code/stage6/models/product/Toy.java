507fd4df16c01099239efab9049f411b7f4acedb
lawrdinca <dincalaurentiu19@stud.ase.ro>
2022-03-28 08:09:59 +0300

    Creating the "Toy" class

---
+++ Seminar_04/src/ro/ase/cts/g1098/clean/code/stage6/models/product/Toy.java
@@ -0,0 +1,11 @@
+package ro.ase.cts.g1098.clean.code.stage6.models.product;
+
+public class Toy  extends Product{
+	
+	int minAge;
+	String description;
+
+	// we can choose the override some of the Product methods or not
+	
+	
+}
