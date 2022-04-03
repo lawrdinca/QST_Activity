9ae7d798edb1ad998afa4af1e09f580da0aa1d6b
laurentiu.dinca <dincalaurentiu19@stud.ase.ro>
2022-04-03 15:28:41 +0300

    adding the missing data

---
+++ Assignment02_DincaLaurentiuGabriel/src/ro/ase/csie/cts/homework1/interfaces/InterestRateInterface.java
@@ -0,0 +1,6 @@
+package ro.ase.csie.cts.homework1.interfaces;
+
+public interface InterestRateInterface {
+
+	public abstract double getMonthlyInterestRate(double loanValue, double interestRate);
+}
